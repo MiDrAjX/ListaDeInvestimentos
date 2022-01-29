22b02014343d8290763162624e94533b93e9411d
Jeison <jeisonmarques78@gmail.com>
2022-01-29 16:38:41 -0300

    adicionando navegação para pagina de overview

--- src/screens/InvestimentList/InvestimentCard/index.tsx
+++ src/screens/InvestimentList/InvestimentCard/index.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+
+import {useNavigation} from '@react-navigation/core';
+
 import * as S from './styles';
 
 export interface InvestimentCardProps {
@@ -6,16 +9,25 @@ export interface InvestimentCardProps {
   objetivo: string;
   saldoTotal: string;
   indicadorCarencia: string;
-  acoes: AcoesProps;
-}
-interface AcoesProps {
-  id: string;
 }
 
 export function InvestimentCard({item}) {
-  const {nome, objetivo, saldoTotal}: InvestimentCardProps = item;
+  const {nome, objetivo, saldoTotal, indicadorCarencia}: InvestimentCardProps =
+    item;
+  const navigation = useNavigation();
+
+  const handleListAcess = useCallback(
+    (investiment, carencia) => {
+      if (carencia !== 'N') {
+        return;
+      }
+      navigation.navigate('Overview', investiment);
+    },
+    [navigation],
+  );
+
   return (
-    <S.Container>
+    <S.Container onPress={() => handleListAcess(item, indicadorCarencia)}>
       <S.ContainerInfo>
         <S.ContainerSpaceBetween>
           <S.TitleText>{nome}</S.TitleText>
