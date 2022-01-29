6988ef75c473bf41e2edbd744eb512739006417f
Jeison <jeisonmarques78@gmail.com>
2022-01-29 16:39:21 -0300

    ajustando tipagens

--- src/screens/InvestimentList/index.tsx
+++ src/screens/InvestimentList/index.tsx
@@ -3,12 +3,16 @@ import React, {useCallback, useEffect, useState} from 'react';
 import api from '../../services/api';
 import {InvestimentCard, InvestimentCardProps} from './InvestimentCard';
 
+import {StocksProps} from '../InvestimentOverview/index';
+
 import uuid from 'react-native-uuid';
 
 import * as S from './styles';
+import {ListRenderItemInfo} from 'react-native';
 
 export interface DataListProps extends InvestimentCardProps {
   id: string;
+  acoes: StocksProps[];
 }
 
 function InvestimentList() {
@@ -48,15 +52,18 @@ function InvestimentList() {
 
   useEffect(() => {
     getInvestiments();
-  }, []);
+  }, [getInvestiments]);
 
   return (
     <S.ContainerList>
       <S.HeaderYellow />
+
       <S.InvestimentFlatList
-        data={investiments}
-        keyExtractor={item => item.id}
-        renderItem={({item}) => <InvestimentCard item={item} />}
+        data={investiments} //perdão não sei resolver esse problema de tipagem T-T
+        keyExtractor={(item: DataListProps) => item.id}
+        renderItem={({item}: ListRenderItemInfo<DataListProps>) => (
+          <InvestimentCard item={item} />
+        )}
         ListHeaderComponent={renderHeader}
         ItemSeparatorComponent={() => <S.Separator />}
       />
@@ -65,22 +72,3 @@ function InvestimentList() {
 }
 
 export default InvestimentList;
-
-/**
- * <S.FlatListReviews
-        data={reviews}
-        ListHeaderComponent={renderHeader}
-        keyExtractor={item => String(item?.id)}
-        renderItem={({item}) => (
-          <ReviewCard navigation={navigation} item={item} />
-        )}
-        showsVerticalScrollIndicator={false}
-        onEndReachedThreshold={0.1}
-        onEndReached={
-          grades.total >= reviews?.length + 1
-            ? () => handleFetchMore(pages)
-            : ''
-        }
-        ListEmptyComponent={renderEmptyList}
-        ListFooterComponent={renderFooter}
-      /> */
